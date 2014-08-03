91048e36e6a92647150446387eba8acfb2e689df
inokasuresh <rmisgeenath@gmail.com>
2014-08-03 22:37:41 +0530

    update inoka

--- src/main/java/com/devicemgt/util/BackendConstants.java
+++ src/main/java/com/devicemgt/util/BackendConstants.java
@@ -11,6 +11,6 @@ public interface BackendConstants {
 	String DELETE = "delete";
 	String DEVICELIST = "deviceList";
 	String USERLIST= "userlist";
-//	String SERVICEURL="https://appserver.dev.cloud.wso2.com/t/manil8056/webapps/devicemanagementser-default-SNAPSHOT/services/devicerepository";
-	String SERVICEURL="http://192.168.42.185:9763/DeviceMgt_Service-1.0.0/services/device_mgt_services";
+	String SERVICEURL="https://appserver.dev.cloud.wso2.com/t/manil8056/webapps/devicemanagementser-default-SNAPSHOT/services/devicerepository";
+//	String SERVICEURL="http://192.168.42.185:9763/DeviceMgt_Service-1.0.0/services/device_mgt_services";
 }
